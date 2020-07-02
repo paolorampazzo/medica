e0b6a1c3c7644a8928ebf2f240d0c678d02c7edd
paolorampazzo <paolorampazzo@gmail.com>
2020-07-01 21:29:09 -0300

    Teste

---
+++ build/precache-manifest.7ec3941b5f8c319a1fff658ea1c9f68f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f52adbe5c3b8ddc54247b6c63b0239",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22916f7ffcb901ce7dbf",
+    "url": "/static/css/main.2618d75f.chunk.css"
+  },
+  {
+    "revision": "8aeaa23e6b671a103e5c",
+    "url": "/static/js/2.53c02ec4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.53c02ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22916f7ffcb901ce7dbf",
+    "url": "/static/js/main.459eb51f.chunk.js"
+  },
+  {
+    "revision": "42d80d0475429a2e1a69",
+    "url": "/static/js/runtime-main.2aa5509b.js"
+  }
+]);
